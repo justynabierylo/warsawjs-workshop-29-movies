4fc584a6df2c4639d948523037b6fe01e0c5ee26
Mac Justyny <Justyna@MacBook-Pro-Mac.lan>
2019-02-26 11:35:29 +0100

    environment update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  moviesUrl: 'http://justynabierylo.github.io/warsawjs-workshop-29-movies/assets/movies.json'
+  moviesUrl: 'https://justynabierylo.github.io/warsawjs-workshop-29-movies/assets/movies.json'
 };
